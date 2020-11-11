7a707fe5379b99f23ae708da4b833c5c9ec9d7cb
shogo takayama <shogo@chalkboard.me>
2020-11-11 22:17:36 +0900

    コードの自動生成

---
+++ producer/README.md
@@ -0,0 +1,3 @@
+## コードの自動生成
+`./mvnw clean compile` を実行することで、target配下にOpenAPI Specificationをもとに自動生成されるコードを吐き出せる
+ 
---
+++ producer/src/main/java/chalkboard/me/cdc/producer/presentation/controller/ReservationController.java
@@ -0,0 +1,6 @@
+package chalkboard.me.cdc.producer.presentation.controller;
+
+import chalkboard.me.cdc.producer.api.ReservationApi;
+
+public class ReservationController implements ReservationApi {
+}
