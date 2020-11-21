79e536a0dd62efdebb2b1613b570ed03e8f53a17
shogo takayama <shogo@chalkboard.me>
2020-11-21 16:55:01 +0900

    post CDC成功

--- producer/src/test/resources/contracts/pizz/ReservationPizzaContract.groovy
+++ producer/src/test/resources/contracts/pizz/ReservationPizzaContract.groovy
@@ -2,17 +2,13 @@ package contracts.pizz
 // https://cloud.spring.io/spring-cloud-contract/2.0.x/multi/multi__contract_dsl.html
 org.springframework.cloud.contract.spec.Contract.make {
     request {
-        method 'GET'
-        urlPath 'reservation/2' // ここを固定しないと、Producer側単体テストの準備が難しい
-    }
-    response {
-        status 200
+        method 'POST'
+        urlPath 'reservation'
         headers {
             header('Content-Type': 'application/json')
         }
         body("""
         {
-          "id": 1234567890,
           "pizza": "ナポリ",
           "topping": [
             "サラミ",
@@ -22,11 +18,8 @@ org.springframework.cloud.contract.spec.Contract.make {
           ]
         }
         """)
-        bodyMatchers {
-            jsonPath('$.id', byRegex('[0-9]{1,10}'))
-            jsonPath('$.pizza', byType())
-            jsonPath('$.topping', byType())
-            jsonPath('$.topping[0]', byType())
-        }
+    }
+    response {
+        status 201
     }
 }
